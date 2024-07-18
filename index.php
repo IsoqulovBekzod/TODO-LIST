92094c43949bcf4647f4d41aebd4cf90c5e45ebe
IsoqulovBekzod <146481317+IsoqulovBekzod@users.noreply.github.com>
2024-07-18 13:05:04 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<form method="POST">
+<input type="checkbox" id="1" name="1" value="Bike">
+<label for="1"> Hello </label><br>
+<input type="checkbox" id="2" name="2" value="Bike">
+<label for="2"> Hello 222</label><br>
+</form>
